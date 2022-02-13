229f139e51099af35e75969bcf3d9c066e1fcbbe
pratikjshinde10@gmail.com <pratikjshinde10@gmail.com>
2022-02-13 12:18:09 +0530

    commit new

--- SimpleAPI/Controllers/WeatherForecastController.cs
+++ SimpleAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace SimpleAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "1test"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "test"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
