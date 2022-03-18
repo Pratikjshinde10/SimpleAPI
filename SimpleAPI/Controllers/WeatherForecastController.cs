37d8a94aecf4be06308da5edd072b54d98ddace6
pratikjshinde10@gmail.com <pratikjshinde10@gmail.com>
2022-03-18 12:32:20 +0530

    commit

--- SimpleAPI/Controllers/WeatherForecastController.cs
+++ SimpleAPI/Controllers/WeatherForecastController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace SimpleAPI.Controllers
@@ -16,23 +18,27 @@ namespace SimpleAPI.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController()
+        private readonly IConfiguration _config;
+      
+        public WeatherForecastController(IConfiguration config)
         {
+            _config = config;
         }      
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            string keyid = _config.GetSection("keyid").Value; 
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            
+            return Enumerable.Range(1, 12).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                Keyid = keyid
             })
-            .ToArray();
+            .ToArray(); 
         }
     }
 }
