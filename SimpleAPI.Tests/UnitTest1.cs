decc03ea75b2858a4556bda49f78397f495008a3
Pratikjshinde10 <31247118+Pratikjshinde10@users.noreply.github.com>
2022-03-18 12:08:46 +0530

    Update UnitTest1.cs

--- SimpleAPI.Tests/UnitTest1.cs
+++ SimpleAPI.Tests/UnitTest1.cs
@@ -1,15 +1,12 @@
 using System;
 using Xunit;
 using SimpleAPI.Controllers;
-using Microsoft.Extensions.Configuration;
 
 namespace SimpleAPI.Tests
 {
     public class UnitTest1
     {
-        private static IConfiguration _config;
-        WeatherForecastController _controller = new WeatherForecastController(_config);
-        
+        WeatherForecastController _controller = new WeatherForecastController(); 
         [Fact]
         public void Test1()
         {
