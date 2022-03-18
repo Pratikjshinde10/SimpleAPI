3f097b128033f114779c5c3a658cbb3952b5b137
Pratikjshinde10 <31247118+Pratikjshinde10@users.noreply.github.com>
2022-03-18 11:39:11 +0530

    Update WeatherForecastController.cs

--- SimpleAPI/Controllers/WeatherForecastController.cs
+++ SimpleAPI/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace SimpleAPI.Controllers
