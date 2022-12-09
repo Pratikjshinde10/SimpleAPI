aba941ab2babbbd6da9242e1eb0fb9f5309682ae
pratikjshinde10@gmail.com <pratikjshinde10@gmail.com>
2022-12-09 20:20:58 +0530

    Auth changes

--- SimpleAPI/Controllers/WeatherForecastController.cs
+++ SimpleAPI/Controllers/WeatherForecastController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Logging;
 
 namespace SimpleAPI.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
