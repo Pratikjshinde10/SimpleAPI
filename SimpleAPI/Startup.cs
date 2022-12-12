eb6e1b47aa259cef805f83301bcae94414573c8c
pratikjshinde10@gmail.com <pratikjshinde10@gmail.com>
2022-12-12 23:40:25 +0530

    f

--- SimpleAPI/Startup.cs
+++ SimpleAPI/Startup.cs
@@ -32,16 +32,18 @@ namespace SimpleAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
 
-            services.AddControllersWithViews(options =>
-            {
-                var policy = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
-                    .Build();
-                options.Filters.Add(new AuthorizeFilter(policy));
-            });
+            services.AddMicrosoftIdentityWebApiAuthentication(Configuration,"AzureAd")
+                    .EnableTokenAcquisitionToCallDownstreamApi().AddInMemoryTokenCaches();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+            // services.AddControllersWithViews(options =>
+            // {
+            //     var policy = new AuthorizationPolicyBuilder()
+            //         .RequireAuthenticatedUser()
+            //         .Build();
+            //     options.Filters.Add(new AuthorizeFilter(policy));
+            // });
 
             services.AddSwaggerGen(c =>
             {
@@ -69,7 +71,7 @@ namespace SimpleAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseAuthentication();
+            //app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
